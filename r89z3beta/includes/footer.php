83fbd4bb8ffacbc844f83231c29eb98c80a173b3
Turnip1234 <51388022+Turnip1234@users.noreply.github.com>
2024-12-24 00:20:12 -0800

    Initial commit

---
+++ r89z3beta/includes/footer.php
@@ -0,0 +1,61 @@
+        <section id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 col-12-medium">
+
+                        <!-- Links -->
+                        <section>
+                            <h2>Links to Important Stuff</h2>
+                            <div>
+                                <div class="row">
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="impressum.html">Impressum</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="pdf/privacy.pdf">Privacy</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="contact.html">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-3 col-12-small">
+                                        <ul class="link-list last-child">
+                                            <li><a href="amazon.html">Amazon Associates</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="col-4 col-12-medium imp-medium">
+
+                        <!-- Blurb -->
+                        <section>
+                            <h2>An Informative Text Blurb</h2>
+                            <p>
+                                Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed.
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright -->
+        <div id="copyright">
+            &copy; EJ Macki LLC except where designated CCA. All rights reserved. | Webiste Design: <a href="http://html5up.net">HTML5 UP</a>
+        </div>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
---
+++ r89z3beta/includes/navigation.php
@@ -0,0 +1,19 @@
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <!-- Logo -->
+                        <h1><a href="index.html" id="logo">EJ Macki, Translator</a></h1>
+
+                        <!-- Nav -->
+                        <nav id="nav">
+                            <a href="index.html">Home</a>
+                            <a href="books.html">Books & News</a>
+                            <a href="resume.html">Résumé</a>
+                            <a href="twocolumn2.html">NCTA</a>
+                            <a href="onecolumn.html">NOTIS</a>
+                        </nav>
+
+                    </div>
+                </div>
+            </div>
